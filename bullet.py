385ee5780b2fee53095a1d27d50f1017978769c8
Danil Guresh <11den110@gmail.com>
2022-10-21 13:11:40 +0300

    Shooting

---
+++ bullet.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """Class for controlling bullets fired by a ship"""
+
+    def __init__(self, ai_settings, screen, ship):
+        """Creates a bullet object at the ship's current position"""
+        super(Bullet, self).__init__()
+        self.screen = screen
+        # Creating a bullet at position (0,0) and assigning the correct position
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+        # Bullet position is stored in real format
+        self.y = float(self.rect.y)
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """Moves the bullet up the screen"""
+        # Update bullet position in real format
+        self.y -= self.speed_factor
+        # Rectangle position update
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Putting a bullet on the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
+        
